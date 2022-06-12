5966451dd654c15df42bcbc5c4263b5c43e63567
Jakub <jaksty1@st.amu.edu.pl>
2022-06-13 00:54:34 +0200

    Add Basic connection with database

--- client2/src/Form.js
+++ client2/src/Form.js
@@ -4,13 +4,33 @@ import FormPositive from './FormPositive'
 
 const Form = () => {
   const [isSubmitted, setIsSubmitted] = useState(false)
+  const [values, setValues] = useState({})
 
-  function submitForm(){
+  function submitForm(isValid, values){
+    if (isValid){
       setIsSubmitted(true);
+      setValues(values);
+      fetch("http://localhost:3001/register", {method: "POST",headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      }, body: JSON.stringify(values)})
+      .then(response => response.json())
+      .then(data => {
+        console.log('Success:', data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+     
+    }else{
+      setIsSubmitted(false);
+      setValues({});
+    }
+      
   }
   return (
     <div>
-        {!isSubmitted ? <FormSignup submitForm={submitForm} /> : <FormPositive />}
+        {!isSubmitted ? <FormSignup submitForm={submitForm} /> : <FormPositive values={values} />}
     </div>
   )
 }
