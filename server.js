5ce13eba54ca053f6a5c77e1ebc6049144b18ef4
pascalbertin <p.bertin@wp.pl>
2022-06-17 17:52:51 +0200

    fixing react routing

--- server.js
+++ server.js
@@ -139,10 +139,7 @@ const teacherSchema = new mongoose.Schema({
 
 
   app.get("*", (req, res) => {
-    let url = path.join(__dirname, '../client/build', 'index.html');
-    if (!url.startsWith('/app/')) // since we're on local windows
-      url = url.substring(1);
-    res.sendFile(url);
+    res.sendFile(`${__dirname}/client/build/index.html`);
   });
 
 
