d9603463568d902eb451541f4876610ece11915f
pascalbertin <p.bertin@wp.pl>
2022-06-18 14:41:33 +0200

    add config file for database connection

--- server.js
+++ server.js
@@ -1,19 +1,11 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 const express = require("express");
-const bcrypt = require("bcrypt");
-const ejs = require("ejs");
 var cors = require('cors');
 const teacherRoute = require("./src/routes/teacherRouter");
+const dbConnectionLink = require("./src/config/databaseConfig");
 
 const app = express();
-
-const dbClusterLink = process.env.DB_CLUSTER_LINK;
-const dbName = process.env.DB_NAME;
-const dbUser = process.env.DB_USER;
-const dbPassword = process.env.DB_PASSWORD;
-
-const dbConnectionLink = "mongodb+srv://" + dbUser + ":" + dbPassword + "@" + dbClusterLink + dbName;
 
 mongoose.connect(process.env.MONGODB_URI || dbConnectionLink, { useNewUrlParser: true });
 mongoose.connection.on('connected', () => {
