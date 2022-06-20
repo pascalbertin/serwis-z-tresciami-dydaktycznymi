1db3077b98b6b708217caa4baaadad5b9d5d7233
Jakub <jaksty1@st.amu.edu.pl>
2022-06-21 01:41:30 +0200

    added code usage

--- src/routes/studentRouter.js
+++ src/routes/studentRouter.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 const studentController = require('../controllers/studentController');
 
-router.route("/code")
+router.route("/codeGenerate")
     .patch(studentController.courseGenerateCode);
 
+router.route("/codeUse")
+    .patch(studentController.courseUseCode);
 
 module.exports = router;
