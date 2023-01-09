c3deaa2569c9c17f3b6e8da16d319ec89daae0a0
Seuch <sebastian.smoluch99@gmail.com>
2023-01-09 22:33:51 +0100

    Update axios.js

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:3001/';
+const BASE_URL = 'https://serwis-z-tresciami.herokuapp.com/';
 
 export default axios.create({
     baseURL: BASE_URL
