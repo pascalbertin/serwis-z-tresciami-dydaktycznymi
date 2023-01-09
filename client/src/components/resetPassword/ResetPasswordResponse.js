bef1a0de12734208ed72e80d36221b8107c577c7
The-Same-One <lukasz.traskowski@gmail.com>
2023-01-09 23:35:49 +0100

    [FIXED] Fixed issues
    - fixed going back to login page
    - fixed categories above slider
    - fixed avatar display

--- client/src/components/resetPassword/ResetPasswordResponse.js
+++ client/src/components/resetPassword/ResetPasswordResponse.js
@@ -1,10 +1,14 @@
-const ResetPasswordResponse = ({msg, success}) => {
+const ResetPasswordResponse = () => {
     const url = "/profile"
   
     return (
+      <div>
       <div className="form-container">
-      {/* {msg ? <h1>{msg}</h1> : <h1>{process.env.REACT_APP_REGISTER_GENERAL_ERROR}</h1>} */}
-      {success ? <div>JEDNORAZOWE HASŁO ZOSTAŁO WYSŁANE NA PODANY ADRES E-MAIL</div> : <div></div>}
+        <div>JEDNORAZOWE HASŁO ZOSTAŁO WYSŁANE NA PODANY ADRES E-MAIL</div>
+      </div>
+        <div className="row">
+          <p className="form-forgot-password"> <a href='/login'>Zaloguj się</a></p>
+        </div>
       </div>
     )
   }
--- client/src/pages/resetPassword/ResetPassword.js
+++ client/src/pages/resetPassword/ResetPassword.js
@@ -3,33 +3,48 @@ import axios from '../../config/axios'
 import { API } from '../../config/api'
 import ResetPasswordForm from '../../components/resetPassword/ResetPasswordForm'
 import ResetPasswordResponse from '../../components/resetPassword/ResetPasswordResponse'
+import {useNavigate, useLocation} from 'react-router-dom'
 
 const ResetPassword = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
   const [isSubmitted, setIsSubmitted] = useState(false)
   const [values, setValues] = useState({})
   const [msg, setMsg] = useState('')
+  const [error, setError] = useState('')
   const [success, setIsSuccess] = useState(false);
 
 
   async function submitForm(isValid, values){
     if (isValid){
       setValues(values)
+      try{
         const response = await axios.patch(API.user, JSON.stringify(values),
         {
           headers: {'Content-Type': 'application/json',
                     'Accept': 'application/json'},
         })
-      setIsSubmitted(true);
-      setValues(values);
-    }
-    else{
+      if (response?.status === 200){
+          setTimeout(navigate('/profile', {state: { from: location}, replace: true}), 1000)
+          navigate(0)
+          setError('Poprawnie zresetowano hasło!')
+      }
+    } catch (err){
+        if(!err?.response) setError('Błąd połączenia')
+        else if(err.response?.status === 400) setError('Email jest wymagany!')
+        else if(err.response?.status === 401) setError('Błąd autoryzacji')
+        else setError('Błąd logowania')
+  }
+  setIsSubmitted(true);
+  setValues(values);
+  }else{
       setIsSubmitted(false);
       setValues({});
     }  
   }
   return (
     <div>
-        {!isSubmitted ? <ResetPasswordForm submitForm={submitForm} /> : <ResetPasswordResponse msg={msg} success={success} />}
+        {!isSubmitted ? <ResetPasswordForm submitForm={submitForm} /> : <ResetPasswordResponse/>}
     </div>
   )
 }
