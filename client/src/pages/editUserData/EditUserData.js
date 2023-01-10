f609b859dd7f9ccab4cd50cea154f2ee47e159c3
The-Same-One <72076235+The-Same-One@users.noreply.github.com>
2023-01-10 04:57:44 +0100

    [ADDED] Added error handling (#125)
    - added error handling for edit user data and money payout

--- client/src/pages/editUserData/EditUserData.js
+++ client/src/pages/editUserData/EditUserData.js
@@ -3,8 +3,12 @@ import axios from '../../config/axios'
 import EditUserDataForm from './EditUserDataForm'
 import { API } from '../../config/api'
 import EditUserDataResponse from './EditUserDataResponse'
+import {useNavigate, useLocation} from 'react-router-dom'
 
 const EditUserData = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [error, setError] = useState('')
   const [isSubmitted, setIsSubmitted] = useState(false)
   const [values, setValues] = useState({})
   const [avatar, setAvatar] = useState(null)
@@ -23,13 +27,25 @@ const EditUserData = () => {
     if (isValid){
       setValues(values);
       setAvatar(avatar);
-      // try{
+      try{
         const response = await axios.patch(API.user + '/' + username, JSON.stringify(values),
         {
           headers: {Authorization: 'Bearer ' + token,
                     'Content-Type': 'application/json'},
           withCredentials: true
-        });
+        })
+        if(response?.status === 200){
+          setTimeout(navigate('/profile', {state: { from: location}, replace: true}), 100)
+          navigate(0)
+          setError('Dane zmieniono poprawnie!')
+        }
+      }catch (err)
+        {
+            if(!err?.response) setError('Błąd połączenia')
+            else if(err.response?.status === 400) setError('Hasło jest wymagane!')
+            else if(err.response?.status === 401) setError('Błąd autoryzacji')
+            else setError('Błąd logowania')
+        }
         setIsSubmitted(true);
         setValues(values);
         uploadAvatar(avatar);
